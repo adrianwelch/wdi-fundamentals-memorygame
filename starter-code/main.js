c6950e598b96a44bfe4f42b938f5d3f02b3c2afc
adrianwelch <adriancwelch@gmail.com>
2017-01-10 15:38:49 +0000

    Unit 9 if rules

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,3 +8,15 @@ var cardThree= "king";
 
 var cardFour= "king";
 
+
+if (card0ne === cardTwo) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
+
+if (cardThree === cardFour) {
+	alert ("You found a match!");
+} else {
+	alert("Sorry, try again.")
+}
